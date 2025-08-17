c4cde6cc9c8429118fe672c3f6fdcf2ad2fc9cc0
Fauza <muhammadfauza27@gmail.com>
2025-08-17 16:29:17 +0800

    update modelling_tuning.py

--- MLProject/modelling_tuning.py
+++ MLProject/modelling_tuning.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, matthews_corrcoef
 import time
+import os
 
 # ========================================
 # Load data preprocessing
