62b73353590aca493c53ef7b17bab9ae487c2dfe
Fauza <muhammadfauza27@gmail.com>
2025-08-17 16:26:34 +0800

    update dashgub token

--- MLProject/modelling_tuning.py
+++ MLProject/modelling_tuning.py
@@ -22,7 +22,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 # Setup MLflow lokal
 # ========================================
 import dagshub
-dagshub.init(repo_owner='Fauza27', repo_name='Eksperimen_SML_Muhammad-Fauza', mlflow=True)
+dagshub.init(repo_owner='Fauza27', repo_name='Eksperimen_SML_Muhammad-Fauza', mlflow=True, token=os.environ["DAGSHUB_TOKEN"])
 
 mlflow.set_tracking_uri("https://dagshub.com/Fauza27/Eksperimen_SML_Muhammad-Fauza.mlflow")
 mlflow.set_experiment("Mine Classification with RF")
